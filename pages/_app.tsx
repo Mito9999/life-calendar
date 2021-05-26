d8763451fbe85c18d8c15a58c5cc24491817f60e
Mito6211 <willmoore674@gmail.com>
2021-05-26 18:01:34 -0400

    Initiate design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const extendedTheme = extendTheme({
+  fonts: { body: "Inter", heading: "Inter", mono: "Inter" },
+  styles: {
+    global: {
+      html: {
+        overflowX: "hidden",
+        overflowY: "scroll",
+      },
+    },
+  },
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Calendar of your Life</title>
+        <meta
+          name="description"
+          content="A visualization of the stages of your life!"
+        />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+        />
+      </Head>
+      <ChakraProvider theme={extendedTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
-export default MyApp
