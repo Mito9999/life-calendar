4b4e4565636115428ccd7c514e78c71a1f5f77cf
Mito6211 <willmoore674@gmail.com>
2021-05-27 20:18:44 -0400

    Add year numbers and refactor headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Flex, Box } from "@chakra-ui/react";
+import { Container, Heading, Flex, Box, Text } from "@chakra-ui/react";
 
 const Circle = ({ size = "20" }: { size: string }) => (
   <svg
@@ -26,15 +26,36 @@ export default function Home() {
       <Heading fontSize="12px" fontWeight="800" textAlign="center" pt="15px">
         Time is limited and precious, how do you want to spend it?
       </Heading>
+      <Heading
+        fontSize="12px"
+        fontWeight="800"
+        pt="15px"
+        pb="8px"
+        width="fit-content"
+      >
+        Weeks of your life
+        <svg
+          width="161"
+          height="8"
+          viewBox="0 0 161 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M160.354 4.35354C160.549 4.15828 160.549 3.84169 160.354 3.64643L157.172 0.464452C156.976 0.26919 156.66 0.26919 156.464 0.464452C156.269 0.659715 156.269 0.976297 156.464 1.17156L159.293 3.99999L156.464 6.82841C156.269 7.02368 156.269 7.34026 156.464 7.53552C156.66 7.73078 156.976 7.73078 157.172 7.53552L160.354 4.35354ZM4.37114e-08 4.5L160 4.49999L160 3.49999L-4.37114e-08 3.5L4.37114e-08 4.5Z"
+            fill="black"
+          />
+        </svg>
+      </Heading>
       <Flex justify="space-between">
         <Heading
           fontSize="12px"
           fontWeight="800"
-          pt="15px"
-          pb="8px"
           width="fit-content"
+          transform="rotate(90deg)"
+          ml="-210px"
+          mb="-170px"
         >
-          Weeks of your life
           <svg
             width="161"
             height="8"
@@ -47,12 +68,13 @@ export default function Home() {
               fill="black"
             />
           </svg>
+          Years of your life
         </Heading>
         <Heading
           fontSize="12px"
           fontWeight="800"
-          mr="-10px"
-          mb="-200px"
+          mr="-60px"
+          mb="-170px"
           width="fit-content"
           transform="rotate(90deg)"
         >
@@ -71,19 +93,35 @@ export default function Home() {
           Stages of your life
         </Heading>
       </Flex>
-      {new Array(100).fill(0).map((_, idx1) => (
-        <Flex mt="2px" maxW="936px" key={`YEAR ${idx1 + 1}`}>
-          {new Array(52).fill(0).map((_, idx2) => (
-            <Box
-              w={936 / 52 - 2 + "px"}
-              mr="2px"
-              key={`YEAR ${idx1 + 1} WEEK ${idx2 + 1}`}
+      <Box display="flex" flexDirection="row">
+        <Flex direction="column" mt="2px">
+          {new Array(100).fill(0).map((_, num) => (
+            <Text
+              fontWeight="700"
+              fontSize="11.5px"
+              ml="-30px"
+              key={"Num" + num}
             >
-              <Circle size="15" />
-            </Box>
+              {num + 1}
+            </Text>
           ))}
         </Flex>
-      ))}
+        <Flex direction="column">
+          {new Array(100).fill(0).map((_, idx1) => (
+            <Flex mt="2px" maxW="936px" key={`YEAR ${idx1 + 1}`}>
+              {new Array(52).fill(0).map((_, idx2) => (
+                <Box
+                  w={936 / 52 - 2 + "px"}
+                  mr="2px"
+                  key={`YEAR ${idx1 + 1} WEEK ${idx2 + 1}`}
+                >
+                  <Circle size="15" />
+                </Box>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
+      </Box>
     </Container>
   );
 }
